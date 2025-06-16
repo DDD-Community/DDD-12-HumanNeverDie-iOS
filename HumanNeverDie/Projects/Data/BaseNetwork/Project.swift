4abef2cebf5cbf7970f341f99175a2e3a41d8b74
kimkyuchul <rbcjf122@gmail.com>
2025-06-16 22:45:47 +0900

    chore: BaseNetwork 의존성 수정

--- HumanNeverDie/Projects/Data/BaseNetwork/Project.swift
+++ HumanNeverDie/Projects/Data/BaseNetwork/Project.swift
@@ -5,6 +5,7 @@ let project = Project.makeModule(
     name: Modules.BaseNetwork.name,
     product: .staticFramework,
     dependencies: [
-        .SPM.alamofire
+        .SPM.alamofire,
+        .shared
     ]
 )
