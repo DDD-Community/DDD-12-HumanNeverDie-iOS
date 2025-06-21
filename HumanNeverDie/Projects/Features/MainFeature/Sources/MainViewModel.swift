67a60bb8c3586523c60564f18d2d60b172165a4d
김규철 <rbcjf122@gmail.com>
2025-06-21 15:48:10 +0900

    feat: TabBar 구조 구현

--- HumanNeverDie/Projects/Features/MainFeature/Sources/MainViewModel.swift
+++ HumanNeverDie/Projects/Features/MainFeature/Sources/MainViewModel.swift
@@ -9,26 +9,16 @@ import Foundation
 import Observation
 
 import CommonFeature
-import MainDomain
-
-import Dependencies
 
 @Observable
 @MainActor
 public final class MainViewModel: ViewModelable {
-  @ObservationIgnored
-  @Dependency(\.mainUseCase) private var mainUseCase
-  
   public struct State: Equatable {
-    var todoData: Todo = .init(id: 0, userId: 0, title: "")
+    var selectedTab: AMDTabBarType  = .home
   }
   
   public enum Action {
-    case onGetTapped
-    case onPostTapped
-    case onPutTapped
-    case onPatchTapped
-    case onDeleteTapped
+    case tabBarItemTapped(AMDTabBarType)
   }
   
   public var state: State = .init()
@@ -36,71 +26,9 @@ public final class MainViewModel: ViewModelable {
   
   public func handleAction(_ action: Action) {
     switch action {
-    case .onGetTapped:
-      Task { await fetchTodo() }
-      
-    case .onPostTapped:
-      Task { await postTodo() }
-      
-    case .onPutTapped:
-      Task { await putTodo() }
-      
-    case .onPatchTapped:
-      Task { await patchTodo() }
-      
-    case .onDeleteTapped:
-      Task { await deleteTodo() }
+    case .tabBarItemTapped(let tab):
+      state.selectedTab = tab
     }
   }
 }
 
-private extension MainViewModel {
-  func fetchTodo() async {
-    do {
-      let todo = try await mainUseCase.fetchTodo(id: 3)
-      state.todoData = todo
-      print("GET 결과:", todo)
-    } catch {
-      print(error)
-    }
-  }
-  
-  func postTodo() async {
-    do {
-      let todo = try await mainUseCase.postTodo(todo: .init(id: 3, userId: 2, title: "dataTest"))
-      state.todoData = todo
-      print("POST 결과:", todo)
-    } catch {
-      print("POST 실패:", error)
-    }
-  }
-  
-  func putTodo() async {
-    do {
-      let todo = try await mainUseCase.putTodo(todo: .init(id: 3, userId: 2, title: "dataTest"))
-      state.todoData = todo
-      print("PUT 결과:", todo)
-    } catch {
-      print("PUT 실패:", error)
-    }
-  }
-  
-  func patchTodo() async {
-    do {
-      let todo = try await mainUseCase.patchTodo(todo: .init(id: 5, userId: 0, title: "Edit"))
-      state.todoData = todo
-      print("PATCH 결과:", todo)
-    } catch {
-      print("PATCH 실패:", error)
-    }
-  }
-  
-  func deleteTodo() async {
-    do {
-      try await mainUseCase.deleteTodo(id: 3)
-      print("DELETE 성공")
-    } catch {
-      print("DELETE 실패:", error)
-    }
-  }
-}
