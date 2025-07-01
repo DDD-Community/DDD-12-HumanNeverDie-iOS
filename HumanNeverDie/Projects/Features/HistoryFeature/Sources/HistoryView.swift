4bf4ca3cea8590ff2e2ca2a946a0b06c83514ecf
liseul <seul_1224@naver.com>
2025-07-01 23:26:30 +0900

    refactor: HistoryView에서 주별 캘린더 제거

--- HumanNeverDie/Projects/Features/HistoryFeature/Sources/HistoryView.swift
+++ HumanNeverDie/Projects/Features/HistoryFeature/Sources/HistoryView.swift
@@ -11,7 +11,6 @@ import CommonFeature
 public struct HistoryView: View {
   @State private var viewModel: HistoryViewModel
   @State private var isMonthPickerPresented = false
-  @State private var isWeekPickerPresented = false
   @State private var tempDate = Date()
   
   public init(viewModel: HistoryViewModel) {
@@ -52,49 +51,7 @@ public struct HistoryView: View {
           }
           .padding()
         }
-
         
-        Color.gray // 선택된 날짜 관련 내용
-        
-        AMDCalendarFactory.createWeekly(
-          currentDate: viewModel.currentDate,
-          sugarIntakeRecordData: viewModel.sugarIntakeRecordData,
-          userSugarTargetValue: 50,
-          selectedDate: $viewModel.selectedDate,
-          onTapTitle: {
-            tempDate = viewModel.selectedDate ?? Date()
-            isWeekPickerPresented = true
-          }
-        ).sheet(isPresented: $isWeekPickerPresented) {
-          VStack(spacing: 20) {
-            DatePicker(
-              "날짜 선택",
-              selection: $tempDate,
-              displayedComponents: [.date]
-            )
-            .datePickerStyle(.wheel)
-            .labelsHidden()
-
-            Button("확인") {
-              viewModel.selectedDate = tempDate
-              isWeekPickerPresented = false
-            }
-
-            Button("닫기") {
-              isWeekPickerPresented = false
-            }
-            .foregroundColor(.red)
-          }
-          .padding()
-        }
-
-        
-        if let selected = viewModel.selectedDate {
-          Text("선택한 날짜: \(DateFormatter.calendarDayFormat.string(from: selected))")
-        } else {
-          Text("날짜를 선택해주세요.")
-            .padding()
-        }
       }
     }
   }
