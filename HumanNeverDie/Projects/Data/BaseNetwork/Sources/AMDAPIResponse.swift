6460cd9bdded58dfbb7a813971bd77f2f36e84d7
김규철 <rbcjf122@gmail.com>
2025-07-10 13:42:05 +0900

    fix: code, status 옵셔널 처리

--- HumanNeverDie/Projects/Data/BaseNetwork/Sources/AMDAPIResponse.swift
+++ HumanNeverDie/Projects/Data/BaseNetwork/Sources/AMDAPIResponse.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct AMDAPIResponse<T: Decodable>: Decodable {
-  public let code: String
-  public let status: Int
+  public let code: String?
+  public let status: Int?
   public let data: T?
 }
