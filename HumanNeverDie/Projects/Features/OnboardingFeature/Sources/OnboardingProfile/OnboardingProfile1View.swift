32a16ecf082c33b1cb3879bafd200b3c08d4dd1d
liseul <seul_1224@naver.com>
2025-07-11 05:03:51 +0900

    feat: 온보딩화면 위치이동, onboardingProfile1View 추가.

---
+++ HumanNeverDie/Projects/Features/OnboardingFeature/Sources/OnboardingProfile/OnboardingProfile1View.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingProfile1View.swift
+//  OnboardingFeature
+//
+//  Created by Seulki Lee on 7/11/25.
+//
+
+import SwiftUI
+
+struct OnboardingProfile1View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+  OnboardingProfile1View()
+}
