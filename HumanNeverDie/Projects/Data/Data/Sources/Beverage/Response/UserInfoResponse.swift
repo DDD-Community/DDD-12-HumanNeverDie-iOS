8b17bc10b93ecdaa3db34e4f447dd52c97373a5b
김규철 <rbcjf122@gmail.com>
2025-08-24 17:44:01 +0900

    fix: 스플래시 -> 온보딩 로직 수정

--- HumanNeverDie/Projects/Data/Data/Sources/Beverage/Response/UserInfoResponse.swift
+++ HumanNeverDie/Projects/Data/Data/Sources/Beverage/Response/UserInfoResponse.swift
@@ -6,28 +6,29 @@
 //
 
 import Foundation
+
 import UserDomain
 
 struct UserInfoResponse: Decodable {
-  let nickname: String
-  let birthDay: String
-  let gender: String
-  let heightCm: Int
-  let weightKg: Int
-  let activityRange: String
-  let sugarIntakeLevel: String
+  let nickname: String?
+  let birthDay: String?
+  let gender: String?
+  let heightCm: Int?
+  let weightKg: Int?
+  let activityRange: String?
+  let sugarIntakeLevel: String?
 }
 
 extension UserInfoResponse {
    public func toDomain() -> UserInfo {
        return UserInfo(
-           nickname: nickname,
-           birthDate: birthDay,
-           selectedGender: Gender(rawValue: gender) ?? .none,
-           height: heightCm,
-           weight: weightKg,
-           selectedActivity: ActivityLevel(rawValue: activityRange) ?? .none,
-           selectedDailySugarGoal: SugarGoal(rawValue: sugarIntakeLevel) ?? .none,
+           nickname: nickname ?? "",
+           birthDate: birthDay ?? "",
+           selectedGender: Gender(rawValue: gender ?? "") ?? .none,
+           height: heightCm ?? 0,
+           weight: weightKg ?? 0,
+           selectedActivity: ActivityLevel(rawValue: activityRange ?? "") ?? .none,
+           selectedDailySugarGoal: SugarGoal(rawValue: sugarIntakeLevel ?? "") ?? .none,
            sugarMaxG: 0,
            sugarIdealG: 0
        )
